607a54f8ca9b6ecd002c8da1f91dcf5c07ee93e7
junaidahmed93@live.com <junaidahmed93>
2016-10-31 15:55:39 +0500

    npm request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var request = require('request');
 
 var app = express();
 
@@ -16,27 +17,51 @@ app.use(express.static(staticDIR));
 
 
 app.get('/slack', function (request, response) {
-  console.log("hitting slack");  
+  console.log("hitting slack");
 })
 
 app.post('/slackPost', function (request, response) {
   console.log(request.body);
-  
+
   response.send({
     "text": "It's 80 degrees right now.", "attachments": [
       {
         "text": "Partly cloudy today and tomorrow"
       }
     ]
-  })  
-  
+  })
+
 })
-app.get('/redirect',function(request,response){
-  console.log("code : " ,request.query.code);
+app.get('/redirect', function (request, response) {
+  console.log("code : ", request.query.code);
+  if (request.query.code) {
+    request({
+      url: 'https://slack.com/api/oauth.access',
+      
+      method: 'POST',
+      headers: {'content-type' : 'application/x-www-form-urlencoded'},
+      // headers: {
+      //   'Content-Type': 'MyContentType',
+      //   'Custom-Header': 'Custom Value'
+      // },
+      body: {
+        client_id : '97506057222.98232203285',
+        client_secret : 'ed83bdf3ccef251efbefc23201402c0d',
+        code : request.query.code
+      }
+    }, function (error, response, body) {
+      if (error) {
+        console.log("error in request" , error);
+      } else {
+        console.log( "response : ", response.statusCode, body);
+      }
+    });
+  }
+
 })
-app.get('*',function(request,response){
-  var indexViewPath = path.resolve(__dirname,"./static/index.html");
-    response.sendFile(indexViewPath);
+app.get('*', function (request, response) {
+  var indexViewPath = path.resolve(__dirname, "./static/index.html");
+  response.sendFile(indexViewPath);
 })
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
