6778edd24477aa1d207014f17e62d0c605248c6f
junaidahmed93 <junaidahmed93@live.com>
2016-11-01 13:29:01 +0500

    response render

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ app.get('/redirect', function (request, response) {
         return console.error('upload failed:', err);
       }
       console.log('Upload successful!  Server responded with:', body);
+      response.render("./static/index");
     });
   }
 
