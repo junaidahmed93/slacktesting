fa23ba6407041207f707b6eeac63dd147869080a
junaidahmed93 <junaidahmed93@live.com>
2016-11-09 17:20:52 +0500

    2

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post('/interactive', function (request, response) {
     if (newParse.actions[0].name == 'Auth Now') {
         console.log("hitting");
         // response.redirect(200, 'https://www.facebook.com/');
+        response.redirect('https://www.facebook.com/');
         response.send({
             "text": "Auth Now Clicked",
             "redirect": "https://www.facebook.com"
@@ -51,12 +52,13 @@ app.post('/interactive', function (request, response) {
     }
     else {
         console.log("not hitting");
-        // response.redirect(200 , 'https://www.facebook.com/');
-
+        // response.redirect(2'https://www.facebook.com/');
+        response.redirect('https://www.facebook.com/');
         response.send({
             "text": "Auth Later clicked",
             "redirect": "https://www.facebook.com"
         })
+        
     }
 })
 
@@ -161,7 +163,6 @@ app.get('/tryme_redirect', function (request, response) {
             console.log('Upload successful!  Server responded with:', body);
         });
     }
-
 })
 
 app.post('/interactive',function(request,response){
